5e89ac82ed4f0da7d0b335954175eb01e4c71a78
Rajesh Medampudi <rajesh@simbotix.com>
2019-11-13 21:16:02 +0530

    styled components implementation

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,12 +8,28 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-
+import { createGlobalStyle } from "styled-components"
 import Header from "./header"
-import "./layout.css"
+// import "./layout.css"
 
 const Layout = ({ children }) => {
-  return <>{children}</>
+  return (
+    <>
+      <GlobalStyle />
+      {children}
+    </>
+  )
 }
 
+const GlobalStyle = createGlobalStyle`
+*{
+  margin:0;
+  padding:0;
+  box-sizing:border-box;
+}
+body{
+  color:#262626;
+  background:#fff;
+}
+`
 export default Layout
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,18 @@
 import React from "react"
-
+import styled from "styled-components"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h3>Hello from Gatsby</h3>
+    <h3>Hello from Gatsby!</h3>
+    <ButtonWrapper>Click Me</ButtonWrapper>
   </Layout>
 )
 
+const ButtonWrapper = styled.button`
+  background: blue;
+  color: white;
+`
 export default IndexPage
