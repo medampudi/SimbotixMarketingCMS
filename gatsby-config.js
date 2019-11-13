3111b166e8766cc9caefdda6d0833c75ff8d1dec
Rajesh Medampudi <rajesh@simbotix.com>
2019-11-13 20:33:37 +0530

    cleanup of starter files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Simbotix Private Limited`,
+    description: `Simbotix is an independent product development agency in the business of developing next-generation solutions to help enterprises to attract and serve more customers. We help customers build out their system from scratch and bring it to market quickly with the help of IaaS providers (AWS, GCP, and Azure) available on the market.`,
+    author: `@simbotix`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,13 +18,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `simbotix`,
+        short_name: `sx`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#fefefe`,
+        theme_color: `#ff6666`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `static/favicon.ico`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
